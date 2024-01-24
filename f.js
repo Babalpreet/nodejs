8a80dd2418a9d084dfe0d29d2e97512f4eda9d26
babal <babalpreet102@gmail.com>
2024-01-24 12:35:00 +0530

    learning git

---
+++ f.js
@@ -0,0 +1,81 @@
+
+//  var,cost,let
+
+// function exampleFunction(){
+//     if(true){
+//         let functionScopedVar="I am a function scoped";
+//         console.log(functionScopedVar);
+//         let functionScopedVar="hi ji";
+//         console.log(functionScopedVar);
+// 5
+        
+//     }
+
+// }
+// exampleFunction();
+// // console.log(functionScopedVar);
+
+
+// shadowing
+
+// let shadowedVar="I am Global";
+// function exampleFunction(){
+//     let shadowedVar="I am local";
+//     console.log(shadowedVar);
+// }
+// exampleFunction();
+// console.log(shadowedVar);
+
+// ----hoisting
+// console.log(x);// undefined
+// // let x =10;
+// console.log(x); //10
+
+// hoisting happens with Var , but it will throw undefined
+// hoisting happens with let , but it will throw a different error
+
+
+// data types
+// let a="hello ji kida";
+// let b=", hor sabh theek ?";
+// console.log(a);
+// console.log(a+b);
+// // console.log(a-b);
+// // console.log(a*5);
+// console.log(typeof(a));
+// let c=1.45,d=2.56;
+// console.log(c+d);
+
+// object
+
+// let person ={
+//     name: "Alice",
+//     age:30,
+//     isStudent:false
+    
+// };
+// console.log(person.name);
+
+
+let car=[ 1,"audi","volvo","VW"];
+console.log(car);
+console.log(car[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ var,let.js
@@ -0,0 +1,84 @@
+// let && const are both block level scoped in const we cannot change its value afterwards
+// var function level scope
+// hositing with var
+
+// function example(){
+//     if(true){
+//         var functionScoped="i am function-scoped";
+//     }
+//     console.log(functionScoped);
+// }
+// example();
+
+// console.log(functionScoped);
+
+
+// function example(){
+//     if(true){
+//         let functionScoped="i am function-scoped";
+//     }
+//     console.log(functionScoped);
+// }
+// example();
+
+// console.log(functionScoped);
+
+
+//--------------re-declatration-----
+// var a=100;
+// var a=150; // re-decleration in var
+
+// let a=10;
+// let a=100; // cnat re-declare in let, ie better constrains
+
+// -------------hoisting
+
+// console.log(x);
+// let x=5;           //let and consst are having same behaviour
+// console.log(x);
+
+
+// console.log(y);
+
+
+// console.log(z);
+// var z=10;
+// console.log(z);
+
+
+// ------------hoisting with function
+
+// hello();
+
+// function hello(){
+//     console.log("Hello, world");
+// }
+
+/// -----------------------object data types
+
+let person ={
+
+    name:"bob",
+    age:30,
+    isStudent: false 
+};
+console.log(person.name);
+console.log(person);
+
+
+//    -----------Array
+ let fruits= [1,"banana","orange"];
+ console.log(fruits[0]);
+
+
+ // ------- type Coercion
+ let result = 5+"5";
+ console.log(result);
+
+ let a="2"+"2";
+ console.log(a);
+ console.log(typeof(a));
+
+ let b="2"+"2"-"2";
+ console.log(b);
+ console.log(typeof(b));
